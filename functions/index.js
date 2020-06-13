39616b658faff5556ea6a3015bffa7a5d5d1cddb
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-06-13 23:40:24 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,26 @@
 const axios = require('axios');
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   const { x, y } = JSON.stringify(event.body);
 
-  axios
-    .post(
+  try {
+    const ok = await axios.post(
       'https://melissa205.000webhostapp.com/recipe/post.php',
       {
         x,
         y,
       },
       { 'Content-Type': 'application/json' },
-    )
-    .then(() => {
-      return {
-        status: 200,
-        message: 'Insert successful.',
-      };
-    })
-    .catch((err) => {
-      console.log(err);
-      return {
-        status: 400,
-        message: 'Bad Request',
-      };
-    });
+    );
+
+    return {
+      status: 200,
+      message: 'Insert successful.',
+    };
+  } catch (e) {
+    return {
+      status: 400,
+      message: 'Bad request.',
+    };
+  }
 };

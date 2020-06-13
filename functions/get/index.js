acb739ef50e029b79df8ad2e7fb1c5ee996b9cf3
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-06-14 00:36:46 +0700

    Add new function

---
+++ functions/get/index.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+const algorithm = (x, y, numPoints) => {
+  let area = 0;
+  let j = numPoints - 1;
+
+  for (let i = 0; i < numPoints; ++i) {
+    area += (x[j] + x[i]) * (y[j] - y[i]);
+    j = i;
+  }
+  return area / 2;
+};
+
+exports.handler = async (event, context, callback) => {
+  try {
+    const { data } = await axios.get(
+      'https://melissa205.000webhostapp.com/recipe/get.php',
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        result: algorithm(
+          data.map(({ x }) => x),
+          data.map(({ y }) => y),
+          data.length,
+        ),
+      }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: 'Bad Request.',
+      }),
+    };
+  }
+};

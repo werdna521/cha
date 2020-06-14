69bbf9b43ce28aefbcf724aa908f8ab402ce1699
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-06-14 14:06:36 +0700

    Update stop function

--- functions/stop/index.js
+++ functions/stop/index.js
@@ -1,8 +1,37 @@
 const axios = require('axios');
 
+const algorithm = (x, y, numPoints) => {
+  let area = 0;
+  let j = numPoints - 1;
+
+  for (let i = 0; i < numPoints; ++i) {
+    area += (x[j] + x[i]) * (y[j] - y[i]);
+    j = i;
+  }
+  return area / 2;
+};
+
 exports.handler = async (event, context, callback) => {
   try {
+    const {
+      data: { data },
+    } = await axios.get('https://melissa205.000webhostapp.com/recipe/get.php');
+
+    const calculated = algorithm(
+      data.map(({ x }) => x),
+      data.map(({ y }) => y),
+      data.length,
+    );
+
     await axios.post('https://melissa205.000webhostapp.com/recipe/clear.php');
+
+    await axios.post(
+      'https://melissa205.000webhostapp.com/recipe/calculate.php',
+      {
+        value: calculated,
+      },
+      { 'Content-Type': 'application/json' },
+    );
 
     return {
       statusCode: 200,
